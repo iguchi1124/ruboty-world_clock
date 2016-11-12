e509e1f541f26e8301cd235bfe1690fee0d49314
Shota Iguchi <shota-iguchi@cookpad.com>
2016-11-12 23:58:40 +0900

    Fix bind message for world clock list

--- lib/ruboty/handlers/world_clock_list.rb
+++ lib/ruboty/handlers/world_clock_list.rb
@@ -1,7 +1,7 @@
 module Ruboty
   module Handlers
     class WorldClockList < Base
-      on /clock list (?<zone>.+)\z/i, name: 'world_clock_list', description: 'Show world clock list'
+      on /list clock (?<zone>.+)\z/i, name: 'world_clock_list', description: 'Show world clock list'
 
       def world_clock(message)
         Ruboty::Actions::WorldClockList.new(message).call
