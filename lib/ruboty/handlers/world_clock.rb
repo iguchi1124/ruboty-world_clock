f8004fda82569343ed31bf77cafc290e5cb8f05e
Shota Iguchi <shota-iguchi@cookpad.com>
2016-11-12 23:21:19 +0900

    Avoid undefined class error

--- lib/ruboty/handlers/world_clock.rb
+++ lib/ruboty/handlers/world_clock.rb
@@ -4,7 +4,7 @@ module Ruboty
       on /clock (?<zone>.+)\z/i, name: 'world_clock', description: 'Show world clock which you want to see'
 
       def world_clock(message)
-        Ruboty::WorldClock::Actions::WorldClock.new(message).call
+        Ruboty::Actions::WorldClock.new(message).call
       end
     end
   end
